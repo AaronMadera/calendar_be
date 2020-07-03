3b3fe1acd0d85a751cce53a6d3c397b33cd53677
AaronMadera <aaron.madera06@gmail.com>
2020-07-03 01:06:18 -0500

    [ADD] Seeder.

---
+++ app/helpers/seeder.js
@@ -0,0 +1,20 @@
+const DB = require('../../lib/db/Db');
+const bcrypt = require('bcryptjs');
+
+global.config = require('../../config.json');
+
+DB.Initialize()
+    .then(async () => {
+        const model = global.db[ 'User' ];
+        const user = {
+            name: 'Admin',
+            email: 'super@user.com',
+            password: bcrypt.hashSync('admin123', bcrypt.genSaltSync(8))
+        };
+        const seederUser = await model.findOne({ email: user.email });
+        if (!seederUser) {
+            new model(user).save();
+            console.log('Seeder User has been created successfully!');
+        } else console.log('Seeder User already exists!')
+    })
+    .catch(e => console.log(e));
