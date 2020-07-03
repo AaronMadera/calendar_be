3512068915bf77207f482105bc166c8bae109525
AaronMadera <aaron.madera06@gmail.com>
2020-07-03 01:06:40 -0500

    [ADD] User token.

--- app/apiController/v1/Users/UsersApiController.js
+++ app/apiController/v1/Users/UsersApiController.js
@@ -1,6 +1,10 @@
+const UserRepository = require('../../../repository/UserRepository');
+const TokenHelper = require('../../../helpers/TokenHelper');
+
 class UsersApiController {
     constructor ($app) {
         this.app = $app;
+        this.userManager = new UserRepository();
     }
     Router(path) {
         this.app.post(`${path}/login`, this.Login.bind(this));
@@ -9,8 +13,16 @@ class UsersApiController {
     async Login(req, res) {
         try {
             const { email, password } = req.body;
-            if (!email || !password) throw new Error('missing fields');
-            res.status(200).json({ error: false, data: { token: {}, user: {} }, msg: 'Logged in successfully' });
+            const config = { condition: { removed: false, email } };
+            const user = await this.userManager.Find(config);
+            if (!user) res.status(401).json({ error: true, msg: 'Wrong credentials, buddy!' });
+
+            const validation = await user.validPass(password);
+            if (validation) {
+                const { email, name, id, _id } = user;
+                const { token, expires } = await TokenHelper.GenerateToken({ email, name, id, _id }, 1);
+                res.status(200).json({ error: false, data: { token, expires, user: { email, name } }, msg: 'Logged in successfully' });
+            } else res.status(401).json({ error: true, msg: 'Wrong credentials, buddy!' })
         } catch (e) {
             res.status(500).json({ error: true, data: {}, msg: 'Error while trying to log in' });
         }
