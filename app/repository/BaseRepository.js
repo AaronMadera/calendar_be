714fe91daa73a7d77bbf99157d0f4072b0b65118
AaronMadera <aaron.madera06@gmail.com>
2020-07-03 01:05:48 -0500

    [ADD] User Repository.

---
+++ app/repository/BaseRepository.js
@@ -0,0 +1,18 @@
+class BaseRepository {
+    constructor (modelName) {
+        this.model = global.db[ modelName ];
+        this.Find = this.Find.bind(this);
+    }
+
+    async Find({ condition = {}, populate = null, sort = null, select = null }) {
+        if (condition.removed === undefined) condition.removed = false;
+        const query = this.model.findOne(condition);
+        if (populate) query.populate(populate);
+        if (sort) query.sort(sort);
+        if (select) query.select(select);
+
+        return query.exec();
+    }
+}
+
+module.exports = BaseRepository;
