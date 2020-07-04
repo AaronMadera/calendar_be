d0db0aba6560895507b1642a0e7bc14307c5f17c
AaronMadera <aaron.madera06@gmail.com>
2020-07-04 14:12:57 -0500

    [FIX] Token.

--- app/helpers/TokenHelper.js
+++ app/helpers/TokenHelper.js
@@ -4,9 +4,8 @@ class TokenHelper {
 
     static async GenerateToken(payload, days) {
         return new Promise((resolve, reject) => {
-            const notBefore = Math.ceil(Date.now() / 1000);
-            const expiresIn = (days * 60 * 60 * 24) + notBefore;
-            jwt.sign(payload, global.config.sessionSecret, { expiresIn, notBefore }, (err, token) => {
+            const expiresIn = (days * 60 * 60 * 24) + Math.ceil(Date.now() / 1000);
+            jwt.sign(payload, global.config.sessionSecret, { expiresIn: `${days}d`, notBefore: '0d' }, (err, token) => {
                 if (err) reject(err);
                 else resolve({
                     token,
